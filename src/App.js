46f6150af764f11df88c2675a6d26127b54336c2
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2025-01-11 18:08:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>    
+    <center><h1><b><i>Hacker's Life </i></b></h1></center>
+    
+   <div class="middle">
+     <ol>
+     <li>
+     <h4><a href="https://copyright.gov.in/SearchRoc.aspx">Crypto Ransomware- Computer Virus (Ransomware) And Its Antivirus.</a></h4>
+     Diary number: 3512/2016-CO/SW
+     <p align="justify">
+     This is a computer Ransomware, copyrighted in the Copyright Office, Govt. of India. This ransomware is undetectable by any Anti-virus Software including but not limited to - Avast, Norton, Guardian, Quick Heal, Kasper Sky, Windows Defender. This Ransomware works perfectly fine in Windows and Linux Based Operating System. Features- This ransomware affects both data present in soft-format, plus permanently damages the hardware.
+     </p>
+     <p>
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>Copyright Office: <a target="_blank" rel="noopener noreferrer" href="http://copyright.gov.in/SearchRoc.aspx">click here</a></li>
+     </ol>
+     </p>
+     </li>
+     <li>
+     <h4><a href="https://copyright.gov.in/SearchRoc.aspx">Cryptbits: Computer Ransomware and its reversal code.</a></h4>
+      Diary number: 6429/2020-CO/SW
+     <p align="justify">
+     This is a computer Ransomware, copyrighted in the Copyright Office, Govt. of India. This ransomware is undetectable by any Anti-virus Software including but not limited to - Avast, Norton, Guardian, Quick Heal, Kasper Sky, Windows Defender. This ransomware written in Java SE works perfectly fine in Windows and Linux Based Operating System.
+     </p>
+     <p>
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>Copyright Office: <a target="_blank" rel="noopener noreferrer" href="http://copyright.gov.in/SearchRoc.aspx">click here</a></li>
+     </ol>
+     </p>
+     </li>
+
+     <li>
+     <h4><a href="https://github.com/mr-ravin/FileCrypt">FileCrypt - an opensource file encryption tool.</a></h4>
+     <p align="justify">
+     FileCrypt is an opensource android application able to perform AES-128 bit encryption on all types of files.
+     <p>
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>App available on: <a href="https://play.google.com/store/apps/details?id=ravin.developer.filecrypt">Google Playstore</a>   </li>
+     <li>Source Code: <a href="https://github.com/mr-ravin/FileCrypt">Github Repository</a></li>
+     </ol>
+     </p>    
+     </p>
+     </li>
+
+     <li>
+     <h4><a href="https://github.com/mr-ravin/EnText">EnText - an opensource text encryption tool.</a></h4>  
+     <p align="justify">
+     An opensource text encryption tool for android using AES-128 bit encryption with CBC, which is considered good for providing security for general use cases, please do consult your cybersecurity adviser if you are looking for a very specific use case.
+     </p>
+     <p>
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>App available on: <a href="https://play.google.com/store/apps/details?id=ravin.developer.entext">Google Playstore</a></li>
+     <li>Source Code: <a href="https://github.com/mr-ravin/entext"> Github Repository</a></li>
+     </ol>
+     </p>
+     </li>
+
+     <li>
+     <h4><a href="https://github.com/mr-ravin/PasswordManager-Android-App">Password Manager - CyberSecure.</a></h4>  
+     <p align="justify">
+     This opensource android application stores all of its data in a 256 bit encrypted format on the device itself, thus it works completely offline.
+     </p>
+     <p>
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>App available on: <a href="https://play.google.com/store/apps/details?id=ravin.developer.passwordmanager">Google Playstore</a> </li>
+     <li>Source Code: <a href="https://github.com/mr-ravin/PasswordManager-Android-App"> Github Repository</a></li>
+     </ol>
+     </p>
+     </li>
+
+     <li>
+     <h4><a href="https://github.com/mr-ravin/ConfiBook-Android-App">ConfiBook - an encrypted book.</a></h4>
+     <p align="justify">
+     This opensource android application stores all of its data in a 256 bit encrypted format on the device itself, thus it works completely offline.
+     </p>
+     <p>
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>App available on: <a href="https://play.google.com/store/apps/details?id=ravin.developer.confidentialbook">Google Playstore</a>   </li>
+     <li>Source Code: <a href="https://github.com/mr-ravin/ConfiBook-Android-App"> Github Repository</a></li>
+     </ol>
+     </p>
+     </li>
+
+     </ol>
+    </div>
+    </div>
+  );
+}
+
+export default App;
